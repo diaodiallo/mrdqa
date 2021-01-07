86016ff4eaad9d35ab7cf82ae8d84c81fa82f1f3
Diao Diallo <diao_diallo@gn.jsi.com>
2021-01-07 19:36:18 +0000

    A few update in supervision page

--- lib/pages/SupervisionPage.dart
+++ lib/pages/SupervisionPage.dart
@@ -25,7 +25,28 @@ class _SupervisionPageState extends State<SupervisionPage> {
         child: MenuManager(context, Routes()).getDrawer(),
       ),
       body: Center(
-        //child: //_supervisionView(),
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 1 / 3,
+              child: Container(
+                color: Colors.amber,
+              ),
+            ),
+            AspectRatio(
+              aspectRatio: 1 / 3,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+            AspectRatio(
+              aspectRatio: 1 / 3,
+              child: Container(
+                color: Colors.deepOrange,
+              ),
+            ),
+          ],
+        ),
         ),
       floatingActionButtonLocation:
       FloatingActionButtonLocation.centerDocked,
@@ -36,10 +57,7 @@ class _SupervisionPageState extends State<SupervisionPage> {
           children: <Widget>[
             FloatingActionButton.extended(
               onPressed: () {
-                setState(() {
-                  Navigator.pushReplacementNamed(
-                      context, Routes().addDataElement);
-                });
+
               },
               label: Text('Facility'),
               icon: Icon(Icons.add),
@@ -47,10 +65,7 @@ class _SupervisionPageState extends State<SupervisionPage> {
             ),
             FloatingActionButton.extended(
               onPressed: () {
-                setState(() {
-                  Navigator.pushReplacementNamed(
-                      context, Routes().addDataElement);
-                });
+
               },
               label: Text('Indicator'),
               icon: Icon(Icons.add),
@@ -60,5 +75,52 @@ class _SupervisionPageState extends State<SupervisionPage> {
         ),
       )
     );
+  }
+
+  Widget _buildSupervisionForm() {
+    final _formKey = GlobalKey<FormState>();
+    return Column(children: [
+      Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Form(
+              key: _formKey,
+              child: Column(children: <Widget>[
+                // Add TextFormFields and ElevatedButton here.
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Designation'),
+                  autofocus: true,
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'The Supervision should have a designation';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Period'),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'The Supervision should have a period';
+                    }
+                    return null;
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, otherwise false.
+                    if (_formKey.currentState.validate()) {
+                      // If the form is valid, display a snackbar. In the real world,
+                      // you'd often call a server or save the information in a database.
+
+                      Scaffold.of(context).showSnackBar(
+                          SnackBar(content: Text('Saving the supervision')));
+                    }
+                  },
+                  child: Text('Save'),
+                ),
+              ])))
+    ]);
   }
 }
